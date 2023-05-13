b4203ca63a6a4131774e6dc5afe83c42346a5339
tejashrirauthub <tejashriraut411@gmail.com>
2023-05-13 17:06:42 +0530

    create hello js
    create hello js

---
+++ hello.js
@@ -0,0 +1,18 @@
+console.log("Tejashri Raut");
+console.log(" best friend name:-Mayuri Bagale..");
+console.log("Address:-At Post medshingi Tal-sangola dist-solapur");
+var  firstName="Tejashri"
+console.log(firstName);
+var age; // Variable Declaration
+age="24"; //Variable Initilization
+console.log(age);
+
+var holder; //variable declaration
+console.log(holder); //undefined
+holder="My Spec"; //variable initilization
+console.log(holder);
+
+holder ="Sachin spec"
+console.log(holder);
+
+
