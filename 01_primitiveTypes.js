d5856de8ef6f8fb6d782b674871da62a03f46fb0
tejashrirauthub <tejashriraut411@gmail.com>
2023-05-14 17:04:54 +0530

    premitive type
    premitive type

---
+++ 01_primitiveTypes.js
@@ -0,0 +1,45 @@
+//prmitive type
+//1.number
+
+var age=31;
+var weight=78;
+var height=6.1;
+var speckNumber=0.50;
+console.log(age,weight,height,speckNumber);
+var typeOfAge=typeof age;
+console.log("type of age:-",typeOfAge);
+console.log("type of specknumber:-",typeof speckNumber);
+console.log("age:-",age);
+console.log("weight:-",weight);
+console.log("height:-",height);
+console.log("speckNumber:-",speckNumber);
+
+
+//2.boolean--->true or false
+var isMarried=true;
+var knowAngular=true;
+var isSoftwareEngineer=true;
+var isBachelor=false;
+console.log("type of is bachlelor-->",isBachelor);
+console.log("type of is married-->",typeof isMarried);
+
+
+//3.string-->sequence of character enclosed in''or ""
+var fisrtName="Bill";
+var position="Microsoft Co-founder";
+var city='';
+console.log("type of city-->",typeof city);
+console.log("type of first name:-",typeof fisrtName);
+console.log("type of position:-",typeof position);
+console.log(80+80);
+
+//2.special data type
+//1.undefined
+var address;
+console.log("address:  ",address);
+console.log("type of address variable-->",typeof address);
+
+//2.null
+var pin=null;
+console.log("value of variable->pin:",pin);
+console.log("type of variable->pin",typeof pin);
